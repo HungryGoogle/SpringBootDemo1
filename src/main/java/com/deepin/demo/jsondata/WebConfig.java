9806c6bb3330e80aea71cfb72c5799da8a171867
LeeHungry <liwenqing2008@126.com>
2018-06-05 20:22:47 +0800

    filter 支持

--- src/main/java/com/deepin/demo/jsondata/WebConfig.java
+++ src/main/java/com/deepin/demo/jsondata/WebConfig.java
@@ -4,9 +4,13 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class WebConfig {
@@ -17,12 +21,23 @@ public class WebConfig {
 
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-
         fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
-
         HttpMessageConverter<?> converter = fastJsonHttpMessageConverter;
-
         return new HttpMessageConverters(converter);
+    }
 
+
+    @Bean
+    public FilterRegistrationBean timeFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+
+        TimeFilter timeFilter = new TimeFilter();
+        registrationBean.setFilter(timeFilter);
+
+        List<String> urls = new ArrayList<>();
+        urls.add("/*");
+        registrationBean.setUrlPatterns(urls);
+
+        return registrationBean;
     }
 }
